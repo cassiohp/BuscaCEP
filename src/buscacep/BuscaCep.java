5895c0160af737752728d6b622ad64fefacdf881
Cássio H <cassio375@gmail.com>
2022-04-18 16:59:00 -0300

    Classe Main Adicionada

--- src/buscacep/BuscaCep.java
+++ src/buscacep/BuscaCep.java
@@ -14,7 +14,8 @@ public class BuscaCep {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TelaBuscaCep tela = new TelaBuscaCep();
+        tela.setVisible(true);
     }
     
 }
